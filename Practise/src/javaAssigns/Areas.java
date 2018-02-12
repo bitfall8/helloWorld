4f8226be5c33162b5713c160f18de5d272870864
bitalpha <anoosh.hari1@gmail.com>
2018-02-12 22:00:22 +0530

    github KT

--- Practise/src/javaAssigns/Areas.java
+++ Practise/src/javaAssigns/Areas.java
@@ -1,14 +1,35 @@
 package javaAssigns;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class Areas {
 	static double pi=22d/7;
-	static void Areas(int a) {
+	static InputStreamReader s=new InputStreamReader(System.in);
+	static BufferedReader br=new BufferedReader(s);
+	static void circle(){
+		int a;
+		System.out.println("enter the value of the radius");
+		a=Integer.parseInt(br.read());
+		System.out.println(a);
 		System.out.println("this is a circle "+pi*a*a);
 	}
-	static void Areas(int a, int b) {
-		System.out.println("this is a rectangle area "+a*b);
+	static void rectangle(){
+		int c,b;
+		System.out.println("enter the length and breadth of the rectangle");
+		c=br.read();
+		b=br.read();
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println("this is a rectangle area "+c*b);
 	}
-	static void Areas(int a, String str) {
-		System.out.println("this is a square "+a*a);
+	static void square(){
+		int d;
+		System.out.println("enter the value of the square");
+		d=br.read();
+		System.out.println(d);
+		System.out.println("this is a square "+d*d);
 	}
 }
--- Practise/src/javaAssigns/Nice.java
+++ Practise/src/javaAssigns/Nice.java
@@ -1,17 +1,19 @@
 package javaAssigns;
 
+import java.io.IOException;
+
 public class Nice {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Perimeters p=new Perimeters();
+//		Perimeters p=new Perimeters();
 		Areas a=new Areas();
 //		p.Perimeters(7);
 //		p.Perimeters(2, 9);
 //		p.Perimeters(4, "square");
-		a.Areas(7);
-		a.Areas(21, 2);
-		a.Areas(16, "hello");
+		a.circle();
+		a.square();
+		a.rectangle();
 	}
 
 }
